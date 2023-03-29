04bb46a22c3229fef28037a4f6eaa476ba7fd152
ehasanulh1 <ehasanulh1@gmail.com>
2023-03-29 10:25:21 +0600

    add mongodb code

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const cors = require('cors');
+require('dotenv').config();
+
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -13,3 +15,12 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
     console.log(`listening to port ${port}`)
 })
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://timeToTravel:h5mOUO57zGqMN695@cluster0.w1kojzp.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+    const collection = client.db("test").collection("devices");
+    // perform actions on the collection object
+    client.close();
+});
